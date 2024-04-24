4cb40441bdceefeee960528abd80062202117108
clanie1 <danybarociov@gmail.com>
2024-04-23 19:53:17 -0600

    init

---
+++ TreeSwiftHackathon/TreeSwiftHackathonApp.swift
@@ -0,0 +1,17 @@
+//
+//  TreeSwiftHackathonApp.swift
+//  TreeSwiftHackathon
+//
+//  Created by Daniel Barocio Valdez on 23/04/24.
+//
+
+import SwiftUI
+
+@main
+struct TreeSwiftHackathonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
