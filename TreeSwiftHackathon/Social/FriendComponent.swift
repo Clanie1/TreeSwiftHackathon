63798c8366fe27ef2004cf3c202ceb338d431831
clanie1 <danybarociov@gmail.com>
2024-04-24 10:46:37 -0600

    Friends

--- TreeSwiftHackathon/Social/FriendComponent.swift
+++ TreeSwiftHackathon/Social/FriendComponent.swift
@@ -8,14 +8,27 @@ struct FriendComponent: View {
     
     var body: some View {
         NavigationLink(destination: FriendProfileView()) {
-            HStack() {
-                Image(systemName: "person").foregroundColor(Color.black)
-                Text("Daniel Barocio").foregroundStyle(Color.black)
-            }.padding(10).border(Color.black)
-        }
+                HStack(alignment: .center) {
+                    ZStack {
+                        Circle()
+                            .fill(Color.white)
+                            .frame(width: 50, height: 50)
+                        Image(systemName: "person").foregroundColor(.black)
+                        Circle().fill(Color.red).frame(width: 12, height:12)
+                        
+                        .foregroundColor(.black).offset(x: 18, y: 18)                 }
+                    .font(.system(size: 20))
+                    VStack (alignment:.leading, spacing: 0){
+                        Text("Daniel Barocio").foregroundStyle(Color.white).font(.title2).fontWeight(.bold)
+                        Text("Desde Abril 2024").foregroundStyle(Color.black)
+                    }.padding(.leading, 10)
+                    Spacer()
+                    Image("Right").resizable().scaledToFit()
+                }.padding(10).clipShape(RoundedRectangle(cornerRadius:10)).frame(height: 70)
+        }.shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/).background(Color.orange).cornerRadius(15)
     }
 }
 
 #Preview {
-    FriendComponent(selectedItem: .none, coins: 10)
+    FriendComponent(selectedItem: .none, coins: 10).background(Color(hex:"#87AFEC"))
 }
--- TreeSwiftHackathon/Social/FriendsView.swift
+++ TreeSwiftHackathon/Social/FriendsView.swift
@@ -2,16 +2,31 @@ import SwiftUI
 
 
 struct FriendsView: View {
+    @State private var search: String = ""
+    
+    
     var body: some View {
         NavigationStack{
             VStack{
                 HStack(){
-                    Text("Amigos").font(.title).fontWeight(.bold)
+                    Text("Amigos").font(.title).fontWeight(.bold).foregroundStyle(Color.white)
                 }
-                Spacer()
-                FriendComponent()
-                FriendComponent()
-            }    }
+                
+                ScrollView{
+                    VStack(spacing:10){
+                        TextField("User name", text: $search).disableAutocorrection(true).onSubmit {
+                            print(search)
+                        }
+                        .padding()
+                        .background(Color(red: 0.95, green: 0.95, blue: 0.95))                        .cornerRadius(5).padding(.bottom, 20)
+                        FriendComponent()
+                        FriendComponent()
+                        FriendComponent()
+                        FriendComponent()
+                        FriendComponent()
+                    }.padding()
+                }.border(Color.gray)
+            }.background(Color(hex:"#87AFEC"))    }
         
     }
 }
