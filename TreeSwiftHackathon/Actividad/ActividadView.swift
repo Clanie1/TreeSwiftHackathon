55b7cf9bec4184afe031220d3afa4874f3c88d85
clanie1 <danybarociov@gmail.com>
2024-04-24 00:05:35 -0600

    chart

--- TreeSwiftHackathon/Actividad/ActividadView.swift
+++ TreeSwiftHackathon/Actividad/ActividadView.swift
@@ -6,10 +6,27 @@
 //
 
 import SwiftUI
+import Charts
 
 struct ActividadView: View {
+    
+    let data = [
+        (name: "Agua", sales: 9631),
+        (name: "Crêpe", sales: 6959),
+        (name: "Injera", sales: 4891)
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment:.center){
+            Text("Actividad").font(.title).fontWeight(.bold).foregroundStyle(Color.white)
+            HStack{
+                Text("1 Mar - 31 Mar 2024").font(.subheadline).foregroundStyle(Color.white)
+            }.frame(height: 20)
+            Chart(data, id: \.name) { name, sales in
+                SectorMark(angle: .value("Value", sales),innerRadius:.ratio(0.9), angularInset: 8)
+                    .foregroundStyle(by: .value("Product category", name)).cornerRadius(8)
+            }.padding(30)
+        }.background(Color(hex: "#1C1C1E"))
     }
 }
 
